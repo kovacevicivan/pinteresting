91f71267ab08d7af4322176471eb66375f61bcd3
Ivan Kovacevic <ivan.kovacevic12@gmail.com>
2013-11-13 01:06:36 +0100

    Moved previous line on the first place

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,9 +84,8 @@ Pinteresting::Application.configure do
   #set paperclip to upload img toamazon s3
   config.paperclip_defaults = {
   :storage => :s3,
-  :bucket => ENV['AWS_BUCKET']
   :s3_credentials => {
-    
+    :bucket => ENV['AWS_BUCKET'],
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
   }
